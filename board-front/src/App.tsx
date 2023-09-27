081a1d9be8180cb81c56ffa7ac96b779ef89ba0a
Hyeonsoo An <ghost00xdg7@naver.com>
2023-09-27 23:59:06 +0900

    CommentItem-app_update

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import './App.css';
 import { latestBoardListMock, top3BoardListMock} from 'mocks';
 import Top3Item from 'components/Top3Item';
+import CommentItem from 'components/CommentItem';
 
 function App() {
   return (
    <>
        {/* latestBoardListMock.map(boardListItem => <BoardItem boardListItem={boardListItem} />) */}
-       <div style={{ display: 'flex', justifyContent: 'center', gap: '24px' }}>
+       {/* <div style={{ display: 'flex', justifyContent: 'center', gap: '24px' }}>
           {top3BoardListMock.map(top3ListItem => <Top3Item top3ListItem={top3ListItem} />)}
+           </div> */}
+       <div>
+        
        </div>
    </>
   );
