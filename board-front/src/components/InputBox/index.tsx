c1d0343b29cf09ac0ae103c23ef2726fe8091729
Hyeonsoo An <ghost00xdg7@naver.com>
2023-10-06 23:23:49 +0900

    InputBox_components

--- board-front/src/components/InputBox/index.tsx
+++ board-front/src/components/InputBox/index.tsx
@@ -1,20 +1,43 @@
-import { forwardRef } from 'react'
+import { ChangeEvent, Dispatch, SetStateAction, forwardRef } from 'react'
 import './style.css';
 
+//          interface: Input Box Properties        //
 interface Props {
+    label: string;
+    type: 'text' | 'password';
+    placeholder: string;
+    value: string;
+    setValue: Dispatch<SetStateAction<string>>;
+    error: boolean;
 
+    icon?: string;
+    onButtonClick?: () => void;
 }
 
+//          component: Input Box 컴포넌트        //
 const InputBox = forwardRef<HTMLInputElement, Props>((props: Props, ref) => {
 
+ //          state: properties        //
+ const { label, type, placeholder, value, error, icon } = props;
+ const { setValue, onButtonClick } = props;
+
+ //          event handler: input 값 변경 이벤트 처리 함수        //
+ const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setValue(value);
+ }
+
+ //          render: Input Box 렌더링        //
  return (
     <div className='inputbox'>
-        <div className='inputbox-label'>{'비밀번호*'}</div>
-        <div className='inputbox-container-error'>
-            <input className='input' />
-            <div className='icon-button'>
-                <div className='icon eye-light-off-icon'></div>
-            </div>
+        <div className='inputbox-label'>{label}</div>
+        <div className={error ? 'inputbox-container-error' : 'inputbox-container'}>
+          <input type={type} className='input' placeholder={placeholder} value={value} onChange={onChangeHandler} />
+          {onButtonClick !== undefined && (
+          <div className='icon-button'>
+          {icon !== undefined && (<div className={`icon ${icon}`}></div>)}
+        </div>
+        )}
         </div>
         <div className='inputbox-message'>{'비밀번호는 8자 이상 입력해주세요.'}</div>
     </div>
