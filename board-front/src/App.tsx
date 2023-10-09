1386a0d2dff76741b984a72727df16f561c73e91
Hyeonsoo An <ghost00xdg7@naver.com>
2023-10-09 23:54:08 +0900

    app tsx edit

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -5,6 +5,7 @@ import Top3Item from 'components/Top3Item';
 import CommentItem from 'components/CommentItem';
 import FavoriteItem from 'components/FavoriteItem';
 import InputBox from 'components/InputBox';
+import Footer from 'layouts/Footer';
 
 function App() {
 
@@ -12,17 +13,18 @@ function App() {
 
   return (
    <>
-       {/* latestBoardListMock.map(boardListItem => <BoardItem boardListItem={boardListItem} />) */}
-       {/* <div style={{ display: 'flex', justifyContent: 'center', gap: '24px' }}>
-          {top3BoardListMock.map(top3ListItem => <Top3Item top3ListItem={top3ListItem} />)}
-           </div> */}
-       {/* <div style={{ padding: '0 20px', display: 'flex', flexDirection: 'column', gap: '30px' }}>
-           {commentListMock.map(commentListItem => <CommentItem commentListItem={commentListItem} />)}
-        </div> */}
-       {/* <div style={{ display: 'flex', columnGap: '30px', rowGap: '20px' }}>
-           {favoriteListMock.map(favoriteListItem => <FavoriteItem favoriteListItem={favoriteListItem} />)}
-        </div> */}
-        <InputBox label='이메일' type='text' placeholder='이메일 주소를 입력해주세요' value={value} error={false} setValue={setValue} message='aaaa'/>
+     {/* latestBoardListMock.map(boardListItem => <BoardItem boardListItem={boardListItem} />) */}
+     {/* <div style={{ display: 'flex', justifyContent: 'center', gap: '24px' }}>
+     {top3BoardListMock.map(top3ListItem => <Top3Item top3ListItem={top3ListItem} />)}
+     </div> */}
+     {/* <div style={{ padding: '0 20px', display: 'flex', flexDirection: 'column', gap: '30px' }}>
+     {commentListMock.map(commentListItem => <CommentItem commentListItem={commentListItem} />)}
+     </div> */}
+     {/* <div style={{ display: 'flex', columnGap: '30px', rowGap: '20px' }}>
+     {favoriteListMock.map(favoriteListItem => <FavoriteItem favoriteListItem={favoriteListItem} />)}
+     </div> */}
+     {/*<InputBox label='이메일' type='text' placeholder='이메일 주소를 입력해주세요' value={value} error={false} setValue={setValue} message='aaaa'/> */}
+     <Footer />
    </>
   );
 }
