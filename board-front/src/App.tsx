f7d36fb28b108e6ac426c12a1d0ad6948f3cf9cf
Hyeonsoo An <ghost00xdg7@naver.com>
2023-10-09 23:58:46 +0900

    front tsx update

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import './App.css';
-import { commentListMock, favoriteListMock, latestBoardListMock, top3BoardListMock} from 'mocks';
-import Top3Item from 'components/Top3Item';
-import CommentItem from 'components/CommentItem';
-import FavoriteItem from 'components/FavoriteItem';
-import InputBox from 'components/InputBox';
-import Footer from 'layouts/Footer';
+////import { commentListMock, favoriteListMock, latestBoardListMock, top3BoardListMock} from 'mocks';
+////import Top3Item from 'components/Top3Item';
+////import CommentItem from 'components/CommentItem';
+////import FavoriteItem from 'components/FavoriteItem';
+////import InputBox from 'components/InputBox';
+////import Footer from 'layouts/Footer';
 
 function App() {
 
