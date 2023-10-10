713c2597fb8a92680c546a416baf1fc61b6c6758
Hyeonsoo An <ghost00xdg7@naver.com>
2023-10-10 23:27:15 +0900

    DomBrower function update

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -1,31 +1,42 @@
-import React, { useState } from 'react';
 import './App.css';
-////import { commentListMock, favoriteListMock, latestBoardListMock, top3BoardListMock} from 'mocks';
-////import Top3Item from 'components/Top3Item';
-////import CommentItem from 'components/CommentItem';
-////import FavoriteItem from 'components/FavoriteItem';
-////import InputBox from 'components/InputBox';
-import Footer from 'layouts/Footer';
+import { Route, Routes } from 'react-router-dom';
 
+import Main from 'views/Main';
+import Authentication from 'views/Authentication';
+import Search from 'views/Search';
+import User from 'views/User';
+import BoardDetail from 'views/Board/Detail';
+import BoardWrite from 'views/Board/Write';
+import BoardUpdate from 'views/Board/Update';
+
+
+
+//          component: Application 컴포넌트          //
 function App() {
 
-  const [value, setValue] = useState<string>('');
-
+  //          render: Application 컴포넌트 렌더링          //
+  // description: 메인 화면 : '/' - Main //
+  // description: 로그인 + 회원가입 화면 : '/auth' - Authentication //
+  // description: 검색 화면 : '/search/:searchWord' - Search //
+  // description: 유저 페이지 : '/user/:userEmail' - User //
+  // description: 게시물 작성하기 : '/board/write' - BoardWrite //
+  // description: 게시물 상세보기 : '/board/detail/:boardNumber' - BoardDetail //
+  // description: 게시물 수정하기 : '/board/update/:boardNumber' - BoardUpdate //
   return (
-   <>
-     {/* latestBoardListMock.map(boardListItem => <BoardItem boardListItem={boardListItem} />) */}
-     {/* <div style={{ display: 'flex', justifyContent: 'center', gap: '24px' }}>
-     {top3BoardListMock.map(top3ListItem => <Top3Item top3ListItem={top3ListItem} />)}
-     </div> */}
-     {/* <div style={{ padding: '0 20px', display: 'flex', flexDirection: 'column', gap: '30px' }}>
-     {commentListMock.map(commentListItem => <CommentItem commentListItem={commentListItem} />)}
-     </div> */}
-     {/* <div style={{ display: 'flex', columnGap: '30px', rowGap: '20px' }}>
-     {favoriteListMock.map(favoriteListItem => <FavoriteItem favoriteListItem={favoriteListItem} />)}
-     </div> */}
-     {/*<InputBox label='이메일' type='text' placeholder='이메일 주소를 입력해주세요' value={value} error={false} setValue={setValue} message='aaaa'/> */}
-     <Footer />
-   </>
+    <Routes>
+      <Route>
+        
+      </Route>
+      <Route path='/' element={<Main />} />
+      <Route path='/auth' element={<Authentication />} />
+      <Route path='/search' element={<Search />} />
+      <Route path='/user' element={<User />} />
+      <Route path='/board'>
+        <Route path='write' element={<BoardWrite />} />
+        <Route path='detail/:boardNumber' element={<BoardDetail />} />  
+        <Route path='update/:boardNumber' element={<BoardUpdate />} />
+      </Route>
+    </Routes>
   );
 }
 
