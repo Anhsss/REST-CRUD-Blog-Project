572bf9c21b86056a5d3adbad5eb9398a3c93a6db
Hyeonsoo An <ghost00xdg7@naver.com>
2023-10-11 20:27:58 +0900

    front-container

--- board-front/src/App.tsx
+++ board-front/src/App.tsx
@@ -1,6 +1,5 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-
 import Main from 'views/Main';
 import Authentication from 'views/Authentication';
 import Search from 'views/Search';
@@ -8,6 +7,8 @@ import User from 'views/User';
 import BoardDetail from 'views/Board/Detail';
 import BoardWrite from 'views/Board/Write';
 import BoardUpdate from 'views/Board/Update';
+import Container from 'layouts/Container';
+import { AUTH_PATH, BOARD_DETAIL_PATH, BOARD_PATH, BOARD_UPDATE_PATH, BOARD_WRITE_PATH, MAIN_PATH, SEARCH_PATH, USER_PATH } from 'constant';
 
 
 
@@ -24,17 +25,17 @@ function App() {
   // description: 게시물 수정하기 : '/board/update/:boardNumber' - BoardUpdate //
   return (
     <Routes>
-      <Route>
-        
-      </Route>
-      <Route path='/' element={<Main />} />
-      <Route path='/auth' element={<Authentication />} />
-      <Route path='/search' element={<Search />} />
-      <Route path='/user' element={<User />} />
-      <Route path='/board'>
-        <Route path='write' element={<BoardWrite />} />
-        <Route path='detail/:boardNumber' element={<BoardDetail />} />  
-        <Route path='update/:boardNumber' element={<BoardUpdate />} />
+      <Route element={<Container />}>
+        <Route path={MAIN_PATH()} element={<Main />} />
+        <Route path={AUTH_PATH()} element={<Authentication />} />
+        <Route path={SEARCH_PATH(':searchWord')} element={<Search />} />
+        <Route path={USER_PATH(':userEmail')} element={<User />} />
+        <Route path={BOARD_PATH()}>
+          <Route path={BOARD_WRITE_PATH()} element={<BoardWrite />} />
+          <Route path={BOARD_DETAIL_PATH(':boardNumber')} element={<BoardDetail />} />  
+          <Route path={BOARD_UPDATE_PATH(':boardNumber')} element={<BoardUpdate />} />
+        </Route>
+        <Route path='*' element={<h1>404 Not Found</h1>} />
       </Route>
     </Routes>
   );
