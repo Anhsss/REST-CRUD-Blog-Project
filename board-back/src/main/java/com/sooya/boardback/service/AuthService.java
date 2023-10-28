afab8a03f60a72cd00f13e53631154b1ecb29ab8
Hyeonsoo An <ghost00xdg7@naver.com>
2023-10-28 22:39:18 +0900

    Auth Service

---
+++ board-back/src/main/java/com/sooya/boardback/service/AuthService.java
@@ -0,0 +1,12 @@
+package com.sooya.boardback.service;
+
+import org.springframework.http.ResponseEntity;
+
+import com.sooya.boardback.dto.request.auth.SignUpRequestDto;
+import com.sooya.boardback.dto.response.auth.SignUpResponseDto;
+
+public interface AuthService {
+
+    ResponseEntity<? super SignUpResponseDto> signUp(SignUpRequestDto dto);
+    
+}
