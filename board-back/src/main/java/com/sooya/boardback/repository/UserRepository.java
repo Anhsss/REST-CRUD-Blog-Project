9ae9e9318fb84ee94d666720b61ca57ebf9322e9
Hyeonsoo An <ghost00xdg7@naver.com>
2023-10-28 22:56:21 +0900

    Query Method update

--- board-back/src/main/java/com/sooya/boardback/repository/UserRepository.java
+++ board-back/src/main/java/com/sooya/boardback/repository/UserRepository.java
@@ -7,5 +7,7 @@ import com.sooya.boardback.entity.UserEntity;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, String> {
+
+    boolean existsByEmail(String email);
     
 }
