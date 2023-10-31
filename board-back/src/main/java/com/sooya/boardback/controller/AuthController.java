3ebd1e928ceed3828ca3c568cb33f94a7ca14c54
Hyeonsoo An <ghost00xdg7@naver.com>
2023-10-31 23:44:58 +0900

    Auth Controller

---
+++ board-back/src/main/java/com/sooya/boardback/controller/AuthController.java
@@ -0,0 +1,32 @@
+package com.sooya.boardback.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sooya.boardback.dto.request.auth.SignUpRequestDto;
+import com.sooya.boardback.dto.response.auth.SignUpResponseDto;
+import com.sooya.boardback.service.AuthService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/sign-up")
+    public ResponseEntity<? super SignUpResponseDto> signUp(
+        @RequestBody @Valid SignUpRequestDto requestBody
+    ) {
+        ResponseEntity<? super SignUpResponseDto> reponse = authService.signUp(requestBody);
+        return reponse;
+    }
+  
+}
