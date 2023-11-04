bd76ac3a64c2dd6cdc20fe3a69a3464b9d0a1f5c
Hyeonsoo An <ghost00xdg7@naver.com>
2023-11-04 21:30:07 +0900

    Auth views

--- board-front/src/views/Authentication/index.tsx
+++ board-front/src/views/Authentication/index.tsx
@@ -4,8 +4,39 @@ import './style.css';
 //          component: 인증 화면 컴포넌트          //
 export default function Authentication() {
 
-  //          render: 인증 화면 렌더링          //
+  //          component: sign in card 컴포넌트          //
+  const SignInCard = () => {
+
+    //          render: sign in card 컴포넌트 렌더링          //
+    return (
+      <div className='auth-card'></div>
+    );
+  };
+
+  //          component: sign up card 컴포넌트          //
+  const SignUpCard = () => {
+
+    //          render: sign Up card 컴포넌트 렌더링          //
+    return (
+      <div className='auth-card'></div>
+    );
+  };
+
+  //          render: 인증 화면 컴포넌트 렌더링          //
   return (
-    <div>인증 화면</div>
+    <div id='auth-wrapper'>
+      <div className='auth-container'>
+        <div className='auth-jumbotron-box'>
+          <div className='auth-jumbotron-contents'>
+            <div className='auth-logo-icon'></div>
+            <div className='auth-jumbotron-text-box'>
+              <div className='auth-jumbotron-text'>{'환영합니다.'}</div>
+              <div className='auth-jumbotron-text'>{'BOARD 입니다.'}</div>
+            </div>
+          </div>
+        </div>
+        <div></div>
+      </div>
+    </div>
   )
 }
