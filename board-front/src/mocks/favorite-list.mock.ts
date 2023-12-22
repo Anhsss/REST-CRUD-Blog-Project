43dcbcca737cba911a3a2176c8a19031b6666c83
Hyeonsoo An <ghost00xdg7@naver.com>
2023-12-22 16:09:06 +0900

    mock update

--- board-front/src/mocks/favorite-list.mock.ts
+++ board-front/src/mocks/favorite-list.mock.ts
@@ -28,6 +28,26 @@ const favoriteListMock = [
         "email": "email@email.com",
         "nickname": "안녕하세요나는주코야키",
         "profileImage": null
+      },
+      {
+        "email": "email@email.com",
+        "nickname": "안녕하세요나는주코야키",
+        "profileImage": null
+      },
+      {
+        "email": "email@email.com",
+        "nickname": "안녕하세요나는주코야키",
+        "profileImage": null
+      },
+      {
+        "email": "email@email.com",
+        "nickname": "안녕하세요나는주코야키",
+        "profileImage": null
+      },
+      {
+        "email": "email@email.com",
+        "nickname": "안녕하세요나는주코야키",
+        "profileImage": null
       }
 ];
 
