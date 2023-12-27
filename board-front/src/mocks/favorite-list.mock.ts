615717660c43365af40cc5ac32269658e465656d
Hyeonsoo An <ghost00xdg7@naver.com>
2023-12-27 20:51:54 +0900

    list edit

--- board-front/src/mocks/favorite-list.mock.ts
+++ board-front/src/mocks/favorite-list.mock.ts
@@ -33,11 +33,6 @@ const favoriteListMock = [
         "email": "email@email.com",
         "nickname": "안녕하세요나는주코야키",
         "profileImage": null
-      },
-      {
-        "email": "email@email.com",
-        "nickname": "안녕하세요나는주코야키",
-        "profileImage": null
       }
 ];
 
