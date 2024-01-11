86f4f9c6439f2ae44e143fd4c5d0191134a80ea4
Hyeonsoo An <ghost00xdg7@naver.com>
2024-01-11 21:10:03 +0900

    favorite update

--- board-front/src/mocks/favorite-list.mock.ts
+++ board-front/src/mocks/favorite-list.mock.ts
@@ -43,7 +43,13 @@ const favoriteListMock = [
         "email": "email@email.com",
         "nickname": "안녕하세요나는주코야키",
         "profileImage": null
-      },      {
+      },      
+      {
+        "email": "email@email.com",
+        "nickname": "안녕하세요나는주코야키",
+        "profileImage": null
+      },
+      {
         "email": "email@email.com",
         "nickname": "안녕하세요나는주코야키",
         "profileImage": null
